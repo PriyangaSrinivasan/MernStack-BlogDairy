4617a1354187c7b2515f77859536d0e66dd511c3
PriyangaSrinivasan <priyalaksha17@gmail.com>
2026-02-09 13:50:17 +0530

    updated

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -2,11 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import blogReducer from "./slices/blogSlice";
 
-const store = configureStore({
+const Store = configureStore({
   reducer: {
     auth: authReducer,
     blogs: blogReducer,
   },
 });
 
-export default store;
+export default Store;
