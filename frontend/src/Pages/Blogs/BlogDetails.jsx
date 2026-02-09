3e8db61ef892efb26f9692d2bce121bf7d17cd02
PriyangaSrinivasan <priyalaksha17@gmail.com>
2026-02-09 13:20:11 +0530

    updated

--- frontend/src/Pages/Blogs/BlogDetails.jsx
+++ frontend/src/Pages/Blogs/BlogDetails.jsx
@@ -8,7 +8,7 @@ import {
   addComment,
   editComment,
   deleteComment,
-} from "../../Redux/Slices/BlogSlice";
+} from "../../Redux/slices/blogSlice";
 
 const BlogDetails = () => {
   const { id } = useParams();
